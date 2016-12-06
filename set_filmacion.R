b9e033b4cd20a69d484ce4d2dba3f389a05192a7
pmtempone <pmtempone@gmail.com>
2016-12-06 01:53:13 -0300

    set filmacion

--- set_filmacion.R
+++ set_filmacion.R
@@ -9,8 +9,8 @@ url_filmacion <- "https://recursos-data.buenosaires.gob.ar/ckan2/set-filmaciones
   
 download.file(url_filmacion,destfile = "BAset.csv")
 
-BAset <- read.csv("~/Documentos/Open_datasets_histories/BAset.csv", sep=";", comment.char="#")
-
+BAset <- read.csv("BAset.csv", sep=";", comment.char="#")
+BAset$latlng <- paste(BAset$LAT,BAset$LNG)
 ----#analisis----
   
 summary(BAset)
@@ -18,3 +18,12 @@ summary(BAset)
 #el barrio que menos pedidos tiene
 
 sort(summary(BAset$BARRIO))
+
+cantidades <- BAset %>% filter(!is.na(latlng)) %>%  
+  mutate(cantidad=as.numeric(latlng)) %>%  
+  group_by(paste(CALLE,ALTURA)) %>%
+  summarise(total.count=n(), 
+            count=sum(is.na(cantidad)))
+
+BAset[BAset$CALLE=="PLAZA" & BAset$ALTURA==3569,"RAZON_SOCIAL"]
+
